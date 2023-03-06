77f5e495f045521371c2f7917ff44f2ffe196385
kosm1n <cc_cosmin@hotmail.com>
2023-03-07 00:11:05 +0100

    feat(dice): tip calculator ok

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import com.example.helloandroid.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
 
--- app/src/main/java/com/example/helloandroid/TipCalculatorActivity.kt
+++ app/src/main/java/com/example/helloandroid/TipCalculatorActivity.kt
@@ -4,17 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.helloandroid.databinding.ActivityMainBinding
+import com.example.helloandroid.databinding.TipCalculatorBinding
 
 class TipCalculatorActivity : AppCompatActivity() {
+
+    private lateinit var binding: TipCalculatorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tip_calculator)
+        binding = TipCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val buttonGoBack: Button = findViewById(R.id.buttonGoBack)
+        binding.buttonCalculate.setOnClickListener{
+            binding.textViewTipCalculatorResult.text = calculateTip().toString()
+        }
 
-        buttonGoBack.setOnClickListener{
+        binding.buttonGoBack.setOnClickListener{
             val intent = Intent(this@TipCalculatorActivity, MainActivity::class.java)
             startActivity(intent)
         }
     }
+
+    private fun calculateTip(): Double {
+        val stringInTextField = binding.editTextNumberDecimalCostOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val tipPercentage = when (binding.tipOptionsPercentage.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var total = cost * (1+tipPercentage)
+        val roundUp = binding.switchRoundUpTip.isChecked
+        if (roundUp) {
+            total = kotlin.math.ceil(total)
+        }
+
+        return total
+    }
+
 }
